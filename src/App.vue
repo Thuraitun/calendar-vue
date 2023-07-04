ef87e477d27930a8068ab3a77e05e7ec431acbf4
Thuraitun <thuraitun64@gmail.com>
2023-07-05 04:59:10 +0630

    calendar app commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen bg-gray-200">
+    <Navbar />
+    <div class="flex">
+      <Calendar />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  import Calendar from './components/Calendar.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Navbar,
+      Calendar
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="py-6 bg-[#42b883]">
+        <nav class="container mx-auto">
+            <div class="flex justify-between">
+                <h2 class="text-2xl font-bold text-gray-100 ms-6">Vue <span class="text-orange-400">Js</span></h2>
+                <h5 class="text-lg font-bold text-gray-100 me-6">Learning <span class="text-orange-400">Beginner</span></h5>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style scoped></style>
